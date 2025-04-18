7a75d513fe44099e3d6b8ea0e807c2e6d00f2270
skels417 <ivan.ivan.parsaev@inbox.ru>
2025-04-18 18:12:38 +0700

    Add files via upload

---
+++ ValveActuatorHMI/ValveActuatorHMI/Classes/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ValveActuatorHMI.Data;
+using ValveActuatorHMI.Services;
+using ValveActuatorHMI.ViewModels;
+
+namespace ValveActuatorHMI
+{
+    public partial class App : Application
+    {
+        public static IModbusService ModbusService { get; private set; }
+        public static ILoggingService LoggingService { get; private set; }
+        public static IExcelService ExcelService { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Initialize services
+            var modbusService = new ModbusService();
+            var excelService = new ExcelService();
+
+            // Initialize main window
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = new MainViewModel(modbusService, excelService);
+            mainWindow.Show();
+        
+    }
+    }
+}
---
+++ ValveActuatorHMI/ValveActuatorHMI/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ValveActuatorHMI.Converters
+{
+    public class BooleanToBrushConverter : IValueConverter
+    {
+        public Brush TrueBrush { get; set; } = Brushes.Green;
+        public Brush FalseBrush { get; set; } = Brushes.Red;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is bool b && b ? TrueBrush : FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
