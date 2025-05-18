3ea2f8a6b652389dcd139f0b7bed46aa267e0877
skels417 <ivan.ivan.parsaev@inbox.ru>
2025-05-18 14:02:31 +0700

    Delete ValveActuatorHMI/ValveActuatorHMI directory

--- ValveActuatorHMI/ValveActuatorHMI/Models/Parameter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ValveActuatorHMI.Models
-{
-    public class Parameter
-    {
-        public string Name { get; set; }
-        public double Value { get; set; }
-        public string Unit { get; set; }
-    }
-}
